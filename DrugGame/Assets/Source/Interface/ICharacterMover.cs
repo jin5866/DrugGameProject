e507d93d80f5af2158f49c01190e74d6e6996f2a
Jin <hjl5866@naver.com>
2017-07-10 02:02:09 +0900

    유니티짱

---
+++ DrugGame/Assets/Source/Interface/ICharacterMover.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Source.Interface
+{
+    interface ICharacterMover
+    {
+        void Move(float h, float v);
+    }
+}
---
+++ DrugGame/Assets/Source/PlayerControl.cs
@@ -0,0 +1,37 @@
+﻿using Assets.Source.Interface;
+using UnityEngine;
+using System.Collections;
+/*
+ * Player의 움직임 관리
+ * 
+ * 2017-07-03 jin5866
+ * 
+ */
+public class PlayerControl : MonoBehaviour {
+    public int playerNum = 1;
+    public float maxSpeed = 10.0f;
+    public float backSpeed = 3.0f;
+
+    public float rotSpeed = 10.0f;
+
+    public MonoBehaviour chaMover;
+
+    private JoyStick joyStick;
+    private ICharacterMover mover;
+
+	// Use this for initialization
+	void Start () {
+        joyStick = GameObject.FindGameObjectWithTag("JoyStick").GetComponent<JoyStick>();
+        mover = (ICharacterMover)chaMover;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //조이스틱 입력값.
+        float h = joyStick.GetHorizontalValue();             
+        float v = joyStick.GetVerticalValue();
+
+        //캐릭터를 움직인다.
+        mover.Move(h, v);
+    }
+}
